e9110d2988ce33c39667a33ccf142fb26ee3c636
virginz <virginiodevito@gmail.com>
2020-11-24 18:02:28 +0100

    'create_db'

--- projectSE/portal/admin.py
+++ projectSE/portal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Competences, SystemUser
 # Register your models here.
+admin.site.register(Competences)
+admin.site.register(SystemUser)
--- projectSE/portal/models.py
+++ projectSE/portal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Competences(models.Model):
+    competencesName = models.CharField(max_length=30)
+    listTask = models.TextField()
+
+    def __str__(self):
+        return(self.competencesName + ' ' + self.listTask)
+
+class SystemUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    role = models.CharField(max_length = 20, choices = [
+        ('SystemAdministrator', 'SystemAdministrator'),
+        ('Planner', 'Planner'),
+        ('Maintainer', 'Maintainer'),
+    ])
+
+    competences = models.ManyToManyField(Competences, blank=True, null=True)
+    
+    def __str__(self):
+        return (self.user.last_name + ' ' + self.user.first_name)
