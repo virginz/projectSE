508f62b5bcfcd22bfd5d77d90945fcbfc0393983
virginz <virginiodevito@gmail.com>
2020-11-27 17:41:45 +0100

    'fix_bug'

--- projectSE/portal/urls.py
+++ projectSE/portal/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views, views_admin, admin_procedures
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/home', login_required(views_admin.AdminView.as_view()), name='systemadministrator_home'),
     path('admin/home/procedures', login_required(admin_procedures.ProcedureView.as_view()), name='systemadministrator_procedures'),
     path('admin/home/add-user/', views_admin.create_user, name='add_user'),
