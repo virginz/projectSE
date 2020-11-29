d934e20d04a358aef0ede345262f617823d54cb3
Antonio Carbone <antoniocarbone@MacBook-Pro.local>
2020-11-29 22:59:14 +0100

    test_forms

---
+++ projectSE/portal/Tests/test_forms.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+from portal.forms import LoginForm, PasswordForm
+
+class TestForms(SimpleTestCase):
+
+    def test_password_form_valide_data(self):
+        form = PasswordForm(data={
+            'password' : 'ingegnere'
+        })
+        self.assertTrue(form.is_valid())
+    
+    def test_login_form_no_data(self):
+        form = LoginForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),2)
+
+"""     def test_login_form_valide_data(self):
+        form = LoginForm(data={
+            'username' : 'paoletto',
+            'password': 'ingegnere'
+        })
+        self.assertTrue(form.is_valid()) """
