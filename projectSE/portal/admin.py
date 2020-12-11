31729b6456f0bd9b6bd72898ad48b28ae6bf65f5
virginz <virginiodevito@gmail.com>
2020-12-11 18:32:26 +0100

    'fix_db'

--- projectSE/portal/admin.py
+++ projectSE/portal/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Competence, Profile, Procedure, Activity, Availability
+from .models import Competence, Profile, Procedure, Activity, Availability, Assignment
 # Register your models here.
 admin.site.register(Competence)
 admin.site.register(Profile)
 admin.site.register(Procedure)
 admin.site.register(Activity)
 admin.site.register(Availability)
+admin.site.register(Assignment)
