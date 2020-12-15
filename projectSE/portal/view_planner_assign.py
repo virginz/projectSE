55dcb930cb596190b6914bab11ff49921563056a
Giuseppe Gaetano <g.gaetano@studenti.unisa.it>
2020-12-15 16:35:37 +0100

    fixed_availability_table

--- projectSE/portal/view_planner_assign.py
+++ projectSE/portal/view_planner_assign.py
@@ -16,11 +16,12 @@ def AssignView(request, pk, week):
     activity = Activity.objects.filter(pk=pk)
     assignment = Assignment.objects.filter(week=week)
     maintainers = Profile.objects.filter(user_type='Maintainer')
-    return render(request, "portal/planner/assign_activity.html", {"list_activity":activity,"list_maintainer":maintainers, "list_assignment":assignment})
+    day_loop = "0123456"
+    return render(request, "portal/planner/assign_activity.html", {"list_activity":activity,"list_maintainer":maintainers, "list_assignment":assignment, "day_loop":day_loop})
 
 
-def ViewAvailabily(request, pkAct, pkAva):
-    availability = Assignment.objects.filter(pk=pkAva)
+def ViewAvailabily(request, pkAct, pkMain, day):
+    availability = Assignment.objects.filter(pk=pkMain)
     activity = Activity.objects.filter(pk=pkAct)
     return render(request, "portal/planner/view_slot.html", {"list_activity":activity, "availability":availability})
 
